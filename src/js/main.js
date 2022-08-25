54e63e0af29a200e6954a578584aeaa879531c45
Krzysztof <krisaczi@yahoo.com>
2022-08-25 22:56:34 +0200

    style cd

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,9 @@
 // }
 
 
+
+// Popup functionality
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
